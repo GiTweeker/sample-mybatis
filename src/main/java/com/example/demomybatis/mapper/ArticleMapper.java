9676879336eb9bf3cfd9190cc39cc98054c2b878
Edih Elijah <richkingpere@yahoo.com>
2022-09-20 15:16:11 +0100

    adding optional @Mapper annotation for intellij support

--- src/main/java/com/example/demomybatis/mapper/ArticleMapper.java
+++ src/main/java/com/example/demomybatis/mapper/ArticleMapper.java
@@ -1,9 +1,10 @@
 package com.example.demomybatis.mapper;
 
 import com.example.demomybatis.pojo.Article;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-
+@Mapper
 public interface ArticleMapper {
     @Select("SELECT * FROM ARTICLES WHERE id = #{id}")
     Article getArticle(@Param("id") Long id);
